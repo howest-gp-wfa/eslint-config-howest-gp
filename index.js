9a2d7faeb17184e80895f8bcf65c3b9df8369fbf
Janiek <janiek.colpaert@howest.be>
2024-04-19 11:46:54 +0200

    Update eslint config

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ module.exports = {
     sourceType: "module"
   },
   rules: {
-    "indent": ["error", 2],
     "no-unused-vars": "warn",
     "no-var": "error",
     "prefer-const": "error",
@@ -18,9 +17,6 @@ module.exports = {
     "semi": ["error", "always"],
     "semi-style": ["error", "last"],
     "semi-spacing": ["error", {"before": false, "after": true}],
-    "space-before-function-paren": ["error", "never"],
-    "space-infix-ops": ["error", { "int32Hint": false }],
-    "spaced-comment": ["error"],
-    "vars-on-top": "error"
+    "space-infix-ops": ["error", { "int32Hint": false }]
   }
 };
